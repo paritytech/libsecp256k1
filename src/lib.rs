f5dd90edb6439a602dd230a031e6046174280f97
Wei Tang <hi@that.world>
2017-11-13 15:49:10 +0800

    public key loading

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,28 @@ mod scalar;
 mod ecmult;
 mod ecdsa;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use field::Field;
+use group::Affine;
+
+pub struct PublicKey([u8; 64]);
+pub struct Signature([u8; 64]);
+
+pub fn public_key_load(pubkey: &PublicKey) -> Affine {
+    let mut ge = Affine::default();
+    let (mut x, mut y) = (Field::default(), Field::default());
+
+    let mut data = [0u8; 32];
+    for i in 0..32 {
+        data[i] = pubkey.0[i];
     }
+    x.set_b32(data.clone());
+    for i in 0..32 {
+        data[i] = pubkey.0[i+32];
+    }
+    x.set_b32(data.clone());
+
+    ge.set_xy(&x, &y);
+    assert!(!ge.x.is_zero());
+
+    ge
 }
