a9a1679be0b6310bc0250ad02598250a1dbc6d93
Bryant Eisenbach <fubuloubu@gmail.com>
2020-02-17 13:38:02 -0700

    review: Change "default" to "debug" to reduce confusion

--- tests/serde.rs
+++ tests/serde.rs
@@ -1,23 +1,23 @@
 use secp256k1::*;
 
 // Public key for secret key: "0101010101010101010101010101010101010101"
-const DEFAULT_PUBLIC_KEY: &str = "\"BBuExVZ7EmRAmV0+1aq6BWXXHhg0YEgZ/5wX9enV3QePcL6vj1iLVBUH/tamQsWrQt/fgSCn9jneUSLUemmo6NE=\"";
+const DEBUG_PUBLIC_KEY: &str = "\"BBuExVZ7EmRAmV0+1aq6BWXXHhg0YEgZ/5wX9enV3QePcL6vj1iLVBUH/tamQsWrQt/fgSCn9jneUSLUemmo6NE=\"";
 
-fn default_public_key() -> PublicKey {
+fn debug_public_key() -> PublicKey {
     let skey = SecretKey::parse(&[1u8; 32]).unwrap();
     PublicKey::from_secret_key(&skey)
 }
 
 #[test]
 fn test_serialize_public_key() {
-    let pkey = default_public_key();
+    let pkey = debug_public_key();
     let serialized_pkey = serde_json::to_string(&pkey).unwrap();
-    assert_eq!(serialized_pkey, DEFAULT_PUBLIC_KEY);
+    assert_eq!(serialized_pkey, DEBUG_PUBLIC_KEY);
 }
 
 #[test]
 fn test_deserialize_public_key() {
-    let serialized_pkey = DEFAULT_PUBLIC_KEY;
+    let serialized_pkey = DEBUG_PUBLIC_KEY;
     let pkey: PublicKey = serde_json::from_str(&serialized_pkey).unwrap();
-    assert_eq!(pkey, default_public_key());
+    assert_eq!(pkey, debug_public_key());
 }
