02d69f636d2bb7c65767bf917475eb7ace634049
Denis P <denis.pisarev@parity.io>
2021-01-15 15:41:34 +0100

    CI: fix GHA deprecations

--- .github/workflows/sccache.ps1
+++ .github/workflows/sccache.ps1
@@ -1,22 +1,22 @@
 #!/usr/bin/env pwsh
-$os=$args[0]
+$OS=$args[0]
 $SCCACHE_CACHE_SIZE="1G"
 $SCCACHE_IDLE_TIMEOUT=0
 $SCCACHE_DIR="$HOME/sccache"
-$version="0.2.12"
-echo "Current OS:" $os
-switch ($os){
-   "macOS" {$platform = "x86_64-apple-darwin"}
-   "Linux" {$platform = "x86_64-unknown-linux-musl"}
-   "Windows"  {$platform ="x86_64-pc-windows-msvc"}
+$VERSION="0.2.13"
+echo "Current OS:" $OS
+switch ($OS){
+   "macOS" {$PLATFORM = "x86_64-apple-darwin"}
+   "Linux" {$PLATFORM = "x86_64-unknown-linux-musl"}
+   "Windows"  {$PLATFORM ="x86_64-pc-windows-msvc"}
 }
-echo "Target arch: " $platform
-$basename = "sccache-$version-$platform"
-$url = "https://github.com/mozilla/sccache/releases/download/"+"$version/$basename.tar.gz"
-echo "Download sccache from "+$url
-curl -LO $url
-tar -xzvf "$basename.tar.gz"
-ls $basename/
-. $basename/sccache --start-server
-echo "::add-path::$(pwd)/$basename"
-echo "::set-env name=RUSTC_WRAPPER::sccache"
+echo "Target arch: " $PLATFORM
+$BASENAME = "sccache-$VERSION-$PLATFORM"
+$URL = "https://github.com/mozilla/sccache/releases/download/"+"$VERSION/$BASENAME.tar.gz"
+echo "Download sccache from "+$URL
+cURL -LO $URL
+tar -xzvf "$BASENAME.tar.gz"
+ls $BASENAME/
+. $BASENAME/sccache --start-server
+echo "$(pwd)/$BASENAME" >> "$GITHUB_PATH"
+echo "RUSTC_WRAPPER=sccache" >> "$GITHUB_ENV"
